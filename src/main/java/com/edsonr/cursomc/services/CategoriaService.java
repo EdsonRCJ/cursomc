dfe055d0b2ae9db9a8f47facea6c5d4341e610cc
EdsonR <ed.juniior01@gmail.com>
2018-06-24 01:02:54 -0300

    Produto e Associacao muitos para muitos

--- src/main/java/com/edsonr/cursomc/services/CategoriaService.java
+++ src/main/java/com/edsonr/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.edsonr.cursomc.domain.Categoria;
 import com.edsonr.cursomc.repositories.CategoriaRepository;
+import com.edsonr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
